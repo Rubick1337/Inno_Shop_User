9241a73f3e0bf4a51ce1053df5f0e745f69f69f9
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:15:42 +0300

    feat: add extasions classes

---
+++ MicroserviceProduct/Inno_Shop_Product/Extasions/Mediator/MediatorExtansion.cs
@@ -0,0 +1,16 @@
+﻿using Application.Products.Commands.CreateProduct;
+
+namespace Inno_Shop_Product.Extasions.Mediator
+{
+    public static class MediatorExtension
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly);
+            });
+            return services;
+        }
+    }
+}
