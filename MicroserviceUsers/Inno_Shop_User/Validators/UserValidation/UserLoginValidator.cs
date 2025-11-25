f7014688dc96267098f5e432eae676b73925e053
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:07:13 +0300

    feat: add validators

---
+++ MicroserviceUsers/Inno_Shop_User/Validators/UserValidation/UserLoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.dto.Auth;
+using FluentValidation;
+
+namespace Inno_Shop_User.Validators.UserValidation
+{
+    public class UserLoginValidator : AbstractValidator<UserLoginDto>
+    {
+        public UserLoginValidator() {
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email обязателен");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Пароль обязателен");
+        }
+    }
+}
