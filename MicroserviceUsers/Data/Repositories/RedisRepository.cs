b4af01ffbb182e89e7627c29d6b5ec2edd5757ec
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:02:31 +0300

    feat: add repositories

---
+++ MicroserviceUsers/Data/Repositories/RedisRepository.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class RedisRepository(IDistributedCache cache) : IRedisRepository
+    {
+        private readonly IDistributedCache _cache = cache;
+
+        public async Task<T?> GetDataAsync<T>(string key)
+        {
+            var data = await _cache.GetStringAsync(key);
+            if (data is null)
+                return default(T);
+
+            return JsonSerializer.Deserialize<T>(data);
+        }
+
+        public async Task SetDataAsync<T>(string key, T value, int minutes = 10)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(minutes)
+            };
+
+            await _cache.SetStringAsync(key, JsonSerializer.Serialize(value), options);
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            await _cache.RemoveAsync(key);
+        }
+    }
+}
