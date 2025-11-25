b8d0cc9cc2c29636c3cd593cc93a0265ea5158d2
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:04:06 +0300

    feat: add dto for models

---
+++ MicroserviceUsers/Application/dto/User/ReadUserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.dto.User
+{
+    public record ReadUserDto(
+        int Id,
+        string Name,
+        string Email,
+        string Role,
+        bool IsActived
+    );
+}
