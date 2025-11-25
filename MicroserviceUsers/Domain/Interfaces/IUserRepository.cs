a330f9f20440ab48b5c38d58983bc2c49d8cdee0
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:02:12 +0300

    feat: add models and interfaces

---
+++ MicroserviceUsers/Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync(
+            string? name = null,
+            string? email = null,
+            string? role = null);
+
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByEmailAsync(string email);
+        Task<User> CreateAsync(User user);
+        Task UpdateAsync(int id, User updatedUser);
+        Task DeleteAsync(int id);
+    }
+}
