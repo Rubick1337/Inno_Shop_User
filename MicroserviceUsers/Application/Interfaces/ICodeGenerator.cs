bbed54f81649351cbab75ab06f5c22c853cb3a96
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:03:51 +0300

    feat: interfaces for services

---
+++ MicroserviceUsers/Application/Interfaces/ICodeGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ICodeGenerator
+    {
+        const int LENGTH = 6;
+        string GenerateCode(int length = LENGTH);
+    }
+}
