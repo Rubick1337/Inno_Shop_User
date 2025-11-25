13af3a02d561050c105af711ffa70cb13d1ab2f3
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:03:34 +0300

    feat: add Services and configuration for jwt

---
+++ MicroserviceUsers/Application/Services/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MailKit.Net.Smtp;        
+
+namespace Application.Services
+{
+    public class EmailService(IConfiguration config) : IEmailService
+    {
+        private readonly IConfiguration _config = config;
+
+        public async Task SendAsync(string to, string subject, string body)
+        {
+            var mailSettings = _config.GetSection("Email");
+
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress("Inno Shop", mailSettings["From"]));
+            email.To.Add(new MailboxAddress("", to));
+            email.Subject = subject;
+            email.Body = new TextPart("html") { Text = body };
+
+            using var smtp = new SmtpClient();
+
+            var port = int.Parse(mailSettings["Port"]);
+            var host = mailSettings["Host"];
+            var from = mailSettings["From"];
+            var password = mailSettings["Password"];
+            var useSsl = bool.Parse(mailSettings["UseSsl"]);
+
+            await smtp.ConnectAsync(host, port, useSsl ? SecureSocketOptions.SslOnConnect : SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(from, password);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+
+
+}
---
+++ MicroserviceUsers/Application/Services/PasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Generate(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+        public bool Verify(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+        }
+    }
+}
