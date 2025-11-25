c7394c99936796ee434ae9bfc4a2fb7e1d020d24
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:06:47 +0300

    feat: add extensions class

---
+++ MicroserviceUsers/Inno_Shop_User/Extensions/Mediator/MediatorExtension.cs
@@ -0,0 +1,17 @@
+﻿using Application.Users.Queries.GetAllUsers;
+
+namespace Inno_Shop_User.Extensions.Mediator
+{
+    public static class MediatorExtension
+    {
+        public static IServiceCollection AddMediator (this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(GetAllUsersQuery).Assembly);
+            });
+            return services;
+        }
+      
+    }
+ }
