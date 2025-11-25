5ae40bd2f75a0db4c0dd5c80726aa0f90f55aee8
Rubick1337 <poss1337@gmail.com>
2025-11-25 18:02:47 +0300

    feat: add migrations and context

---
+++ MicroserviceUsers/Data/Context/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using Data.Context.Configuration;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Week_3_Inno_PreTrainee.Data.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+               : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MicroserviceUsers/Data/Migrations/20251120212853_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    IsActived = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "IsActived", "Name", "PasswordHash", "Role" },
+                values: new object[,]
+                {
+                    { 1, "admin@gmail.com", true, "Admin", "123456", "Admin" },
+                    { 2, "user@gmail.com", true, "User", "654321", "User" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
