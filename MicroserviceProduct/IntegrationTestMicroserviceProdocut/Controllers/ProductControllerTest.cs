ef6643c218ee7ac285048a16eb970953a27d2b59
Rubick1337 <poss1337@gmail.com>
2025-11-30 23:02:52 +0300

    feat: add integration test for product and patrial class for test program cs

---
+++ MicroserviceProduct/IntegrationTestMicroserviceProdocut/Controllers/ProductControllerTest.cs
@@ -0,0 +1,86 @@
+﻿using Application.Dto.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Inno_Shop_Product;
+
+namespace IntegrationTestMicroserviceProdocut.Controllers
+{
+    public class ProductControllerTest
+    {
+        [Fact]
+        public async Task GetAllProduct_Should_ReturnProducts()
+        {
+            var response = await _client.GetAsync("/api/products");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var products = await response.Content.ReadFromJsonAsync<List<ReadProductDto>>();
+            Assert.NotNull(products);
+        }
+        [Fact]
+        public async Task GetById_NotExistingProduct_NotFound()
+        {
+            using var factory = new WebApplicationFactory<Program>();
+            using var client = factory.CreateClient();
+
+            var response = await client.GetAsync("/api/products/999999");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        [Fact]
+        public async Task GetById_Should_ReturnProduct()
+        {
+            using var factory = new WebApplicationFactory<Program>();
+            using var client = factory.CreateClient();
+
+            var response = await client.GetAsync("/api/products/3");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var product = await response.Content.ReadFromJsonAsync<ReadProductDto>();
+            Assert.NotNull(product);
+        }
+        [Fact]
+        public async Task CreateProduct_WithoutAuth()
+        {
+            using var factory = new WebApplicationFactory<Program>();
+            using var client = factory.CreateClient();
+
+            var newProduct = new CreateProductDto(
+                "Мышка",
+                "Мышка игровая",
+                590m,
+                true
+            );
+            var response = await client.PostAsJsonAsync("/api/products", newProduct);
+
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+        [Fact]
+        public async Task DeleteProduct_WithoutAuth()
+        {
+            using var factory = new WebApplicationFactory<Program>();
+            using var client = factory.CreateClient();
+
+            var response = await client.DeleteAsync("/api/products/1");
+
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+        [Fact]
+        public async Task SoftDeleteProduct_Should_Return()
+        {
+            using var factory = new WebApplicationFactory<Program>();
+            using var client = factory.CreateClient();
+
+            var response = await client.PatchAsync("/api/products/1/softdelete", content: null);
+
+            Assert.NotEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
