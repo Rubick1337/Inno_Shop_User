37fa13ac0b35d951689303350f1402c5006b4710
Rubick1337 <poss1337@gmail.com>
2025-12-01 00:07:46 +0300

    feat: add Extension class

--- MicroserviceUsers/Inno_Shop_User/Program.cs
+++ MicroserviceUsers/Inno_Shop_User/Program.cs
@@ -22,11 +22,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient("Products", client =>
-{
-    var baseUrl = configuration["Services:ProductsBaseUrl"];
-    client.BaseAddress = new Uri(baseUrl);
-});
+builder.Services.AddHttp(configuration);
 builder.Services.AddServices();
 builder.Services.AddRepositories();
 
