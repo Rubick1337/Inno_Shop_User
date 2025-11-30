cba02330a2acfaed940a2989c3b4f2185b9687ba
Rubick1337 <poss1337@gmail.com>
2025-11-30 15:06:46 +0300

    feat: add unit test for getby id and all

---
+++ UnitTestsMicroserviceProduct/Products/Queries/GetByIdProductHandlerTest.cs
@@ -0,0 +1,40 @@
+﻿using Application.Products.Queries.GetByIdProduct;
+using Domain.Interfaces;
+using Domain.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsMicroserviceProduct.Products.Queries
+{
+    public class GetByIdProductHandlerTest
+    {
+        [Fact]
+        public async Task GetByIdProduct_Should_Get_Product()
+        {
+            var productRepositoryMock = new Mock<IProductRepository>();
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Мышка",
+                Description = "Мышка игровая",
+                Price = 590,
+                IsAvailable = true,
+                IsDeleted = false,
+                UserId = 1,
+                CreatedAt = new DateTime(2025, 11, 11, 0, 0, 0, DateTimeKind.Utc)
+            };
+            productRepositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(product);
+            var query = new GetProductByIdQuery(10);
+            var handler = new GetProductByIdQueryHandler(productRepositoryMock.Object);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            productRepositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<int>()),Times.Once());
+            Assert.Equal("Мышка", result.Name);
+        }
+    }
+}
